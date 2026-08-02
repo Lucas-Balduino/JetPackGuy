09a4919e98e32da1c1c525e68163a8ca89ac1339
Lucas <lucas.balduino@sempreceub.com>
2026-08-01 22:34:24 -0300

    fix: remove duplicate checkCollision declaration (use entities.checkCollision)

--- src/main.js
+++ src/main.js
@@ -88,14 +88,7 @@ function getRandomFloat(min, max) {
   return Math.random() * (max - min) + min;
 }
 
-function checkCollision(rect1, rect2) {
-  return (
-    rect1.x < rect2.x + rect2.width &&
-    rect1.x + rect1.width > rect2.x &&
-    rect1.y < rect2.y + rect2.height &&
-    rect1.y + rect1.height > rect2.y
-  );
-}
+// `checkCollision` imported from `src/entities.js`
 
 function checkAllCollisions() {
   const p = playerEntity.state;
